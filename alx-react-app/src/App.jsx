b775ff34a6c7760899654bc331d9a86ad9dfe6ad
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-02-19 14:52:54 +0100

    Adiing components to the app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,13 @@
-import WelcomeMessage from "./assets/WelcomeMessage"
-
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 function App() {
     return(
-      <WelcomeMessage />
+      <>
+      <Header />
+      <MainContent />
+      <Footer />
+      </>
     )
 }
 
