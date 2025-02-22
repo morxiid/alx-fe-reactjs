6589a9abe762250186f44d434deec3e53cb5c5ac
El Morchid Aymane <morxiidbullishly2gmail.com>
2025-02-22 23:41:47 +0100

    Applying inline css styles to thhe components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer style={{color: 'green', border: 'black 1px solid'}}>
+            <p>&copy; 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer
