2c336261c18e1a397af0fe1cd0a2a3ee104428c2
El Morchid Aymane <morxiidbullishly2gmail.com>
2025-02-23 16:08:03 +0100

    Added Counter component to App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,10 +1,10 @@
-import UserProfile from "./components/UserProfile"
+import Counter from "./components/Counter";
+
+
 function App() {
     return(
       <>
-          <UserProfile name='Alice' age='25' 
-          bio='Loves hiking and photography' />
-          
+        <Counter />
       </>
     );
 };
