20796c38592c211ddca86747f34ce141880f810c
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-02-25 23:25:39 +0000

    Adding css styles to navigation bar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <div>
-            <nav>
+            <nav style={{display:"flex", justifyContent:"space-between",
+                alignItems:"center", backgroundColor:"#333", color:"white"
+            }}>
                 <Link to="/Home">Home</Link> {" "}
                 <Link to="/About">About</Link> {" "}
                 <Link to="/Services">Services</Link> {" "}
