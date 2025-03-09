95ac209420a38c69d4e646c096c2a30156c9c4e0
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-09 23:38:05 +0000

    creating recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,11 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  <>
+    <RecipeList />
+    <AddRecipeForm />
+  </>
+}
+
+export default App
