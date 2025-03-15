deb95f9702fb2064b4093e67a4e822357c70844b
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-15 15:33:07 +0000

    Updating file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 import useRecipeStore from './useRecipeStore';
 
 const RecipeList = () => {
@@ -11,7 +12,10 @@ const RecipeList = () => {
         <ul>
           {filteredRecipes.map((recipe) => (
             <li key={recipe.id}>
-              <h3>{recipe.name}</h3>
+              {/* Wrap the recipe name in a Link to navigate to the recipe details */}
+              <Link to={`/recipes/${recipe.id}`}>
+                <h3>{recipe.name}</h3>
+              </Link>
               <p>{recipe.description}</p>
               <p>Preparation Time: {recipe.preparationTime} minutes</p>
               <p>Ingredients: {recipe.ingredients}</p>
