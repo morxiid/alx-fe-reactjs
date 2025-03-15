f1febf029d15e3632df09dbf677f5ed60b6dc27d
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-15 15:35:15 +0000

    addingg routes to app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import SearchBar from './SearchBar';
-import Filters from './Filters';
-import RecipeList from './RecipeList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing components
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail'; // Import RecipeDetail component
+import AddRecipeForm from './components/AddRecipeForm'; // Import AddRecipeForm component
+import EditRecipeForm from './components/EditRecipeForm'; // Import EditRecipeForm component
+import Home from './components/Home'; // Import Home component (if you have one)
 
-const RecipeApp = () => {
+function App() {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <SearchBar />
-      <Filters />
-      <RecipeList />
-    </div>
-  );
-};
+    <Router>
+      <Routes>
+        {/* Home Route */}
+        <Route path="/" element={<Home />} />
 
-export default RecipeApp;
+        {/* Recipe List Route */}
+        <Route path="/recipes" element={<RecipeList />} />
+
+        {/* Add Recipe Route */}
+        <Route path="/recipes/add" element={<AddRecipeForm />} />
+
+        {/* Recipe Detail Route */}
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+
+        {/* Edit Recipe Route */}
+        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
