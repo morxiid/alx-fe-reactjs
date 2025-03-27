90358df1bb7205b6b4fd23a85a7175c843a3298a
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-27 00:10:35 +0000

    donee

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import RegistrationFform from "./components/RegistrationForm"
+import FormikForm from "./components/formikForm"
+function App() {
+  
+  return(
+    <>
+    <FormikForm/>
+    <hr />
+    
+    </>
+  )
+}
+
+export default App
