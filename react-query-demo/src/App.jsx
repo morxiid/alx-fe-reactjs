c47bfaeaadd56e67b67bdae00d39e915fb0a318d
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-28 22:39:09 +0000

    kkkkkkkk

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,12 @@
-import PostsComponent from './PostsComponent'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './components/PostsComponent';
+  const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <div className="app">
-      <PostsComponent />
-    </div>
-  )
-}
-
-export default App
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+    );
+  }
+  export default App
