a2646d61d21b8e30c26a614433a55d7daf4053ad
Morchiid <morxiidbullishly@gmail.com>
2025-04-12 19:41:41 +0100

    user profile styling

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,8 @@
+import UserProfile from "./components/UserProfile";
+
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 drop-shadow-lg">Tailwind is glowing 💙</h1>
-
-    </div>
+    <UserProfile/>
   );
 }
