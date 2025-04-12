1ad695b5ae38468e852d2db541cdf23b9ea376eb
Morchiid <morxiidbullishly@gmail.com>
2025-04-12 01:13:12 +0100

    react tailwind instalation

---
+++ tailwind-react-integration/postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {
+        tailwindConfig: './tailwind.config.js',
+      },
+      autoprefixer: {},
+    },
+  };
+  
---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 drop-shadow-lg">Tailwind is glowing 💙</h1>
+
+    </div>
+  );
+}
